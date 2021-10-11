0d474aa196fb24d3d00e0294d2777642366f6c92
Nicolas Lorusso <56365551+lorux0@users.noreply.github.com>
2021-10-11 12:33:26 -0300

    feat: update profile description from a unity-renderer message (#77)

--- packages/unity-interface/BrowserInterface.ts
+++ packages/unity-interface/BrowserInterface.ts
@@ -309,6 +309,10 @@ export class BrowserInterface {
   public SaveUserUnverifiedName(changes: { newUnverifiedName: string }) {
     store.dispatch(saveProfileRequest({ unclaimedName: changes.newUnverifiedName }))
   }
+  
+  public SaveUserDescription(changes: { description: string }) {
+    store.dispatch(saveProfileRequest({ description: changes.description }))
+  }
 
   public CloseUserAvatar(isSignUpFlow = false) {
     if (isSignUpFlow) {
