1e5a961b5e3f1443059a5649e63529c3a96439f3
Pato <pato@decentraland.org>
2021-10-20 10:40:04 -0300

    feat: add support for new input keys

--- packages/unity-interface/BrowserInterface.ts
+++ packages/unity-interface/BrowserInterface.ts
@@ -161,6 +161,15 @@ export class BrowserInterface {
     const scene = getSceneWorkerBySceneID(data.sceneId)
     if (scene) {
       scene.emit(data.eventType as IEventNames, data.payload)
+
+      // Keep backward compatibility with old scenes using deprecated `pointerEvent`
+      if (data.eventType === 'actionButtonEvent') {
+        const { payload } = data.payload
+        // CLICK, PRIMARY or SECONDARY
+        if (payload.buttonId >= 0 && payload.buttonId <= 2) {
+          scene.emit('pointerEvent', data.payload)
+        }
+      }
     } else {
       if (data.eventType !== 'metricsUpdate') {
         defaultLogger.error(`SceneEvent: Scene ${data.sceneId} not found`, data)
@@ -309,7 +318,7 @@ export class BrowserInterface {
   public SaveUserUnverifiedName(changes: { newUnverifiedName: string }) {
     store.dispatch(saveProfileRequest({ unclaimedName: changes.newUnverifiedName }))
   }
-  
+
   public SaveUserDescription(changes: { description: string }) {
     store.dispatch(saveProfileRequest({ description: changes.description }))
   }
