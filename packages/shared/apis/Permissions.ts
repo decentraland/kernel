2f6b6f0e8cdfd1aa7f1139a7e5cc5d608598a638
Suduck <adrian@suduck.com>
2021-12-20 12:49:12 +0100

    Squashed commit of the following:
    commit 473692f013cc875ba6e35604a9e756292d592912
    Author: Gon Pombo <gonzalo@decentraland.org>
    Date:   Fri Dec 17 11:56:16 2021 -0300
    
        chore: refactor allscene events. Move to a saga and add some tests (#130)
    
    commit 62e4f6af972a8b28842e98ef2ce01a38c307a573
    Author: Leandro Mendoza <lea.mendoza97@gmail.com>
    Date:   Thu Dec 16 18:01:12 2021 -0300
    
        fix: hasPermissions function when it's a portable experience. (#131)
    
    commit 18ab21c362e192cc006b12a6dc544a5a5dc32e4c
    Author: Gon Pombo <gonzalo@decentraland.org>
    Date:   Thu Dec 16 16:43:14 2021 -0300
    
        chore: add dependabot for dcl packages (#132)
    
    commit 5c0c71c9d1390e7f66af1e92615ad6d87052d19b
    Author: Leandro Mendoza <lea.mendoza97@gmail.com>
    Date:   Thu Dec 16 09:32:27 2021 -0300
    
        feat: add permissions module (#124)
    
        * feat: add permissions module and implement first blocked action for portable experiences (web3)
    
        * remove default permissions for portable experience
    
        * add RestrictedExposableAPI class
    
        * remove unused imports
    
        * implement restrictedExposableAPI to RestrictedActions
    
        * fix test
    
        * derogate permissiosn in portable experiences.
    
        * change ensure => assert
    
    commit 9beb77eca224b4af927f033387cddac180405e8e
    Author: Leandro Mendoza <lea.mendoza97@gmail.com>
    Date:   Thu Dec 16 09:17:20 2021 -0300
    
        feat: add xmas and halloween empty scenes and a configuration from makefile to switch between them (#129)

---
+++ packages/shared/apis/Permissions.ts
@@ -0,0 +1,61 @@
+import { registerAPI, exposeMethod } from 'decentraland-rpc/lib/host'
+import { ExposableAPI } from './ExposableAPI'
+import { ParcelIdentity } from './ParcelIdentity'
+
+export enum PermissionItem {
+  ALLOW_TO_MOVE_PLAYER_INSIDE_SCENE = 'ALLOW_TO_MOVE_PLAYER_INSIDE_SCENE',
+  ALLOW_TO_TRIGGER_AVATAR_EMOTE = 'ALLOW_TO_TRIGGER_AVATAR_EMOTE',
+  USE_WEB3_API = 'USE_WEB3_API'
+}
+
+export const defaultParcelPermissions = [PermissionItem.USE_WEB3_API]
+export const defaultPortableExperiencePermissions = []
+
+@registerAPI('Permissions')
+export class Permissions extends ExposableAPI {
+  parcelIdentity = this.options.getAPIInstance(ParcelIdentity)
+  protected permissionGranted: PermissionItem[] = defaultParcelPermissions
+
+  /**
+   * Returns if it has a specific permission
+   */
+  @exposeMethod
+  async hasPermission(test: PermissionItem): Promise<boolean> {
+    // Backward compatibility with parcel scene with 'requiredPermissions' in the scene.json
+    //  Only the two permissions that start with ALLOW_TO_... can be conceed without user
+    //  interaction
+    if (this.parcelIdentity.land) {
+      const json = this.parcelIdentity.land.sceneJsonData
+      const list = (json?.requiredPermissions || []) as PermissionItem[]
+
+      if (
+        list.includes(test) &&
+        (test === PermissionItem.ALLOW_TO_MOVE_PLAYER_INSIDE_SCENE ||
+          test === PermissionItem.ALLOW_TO_TRIGGER_AVATAR_EMOTE)
+      ) {
+        return true
+      }
+    }
+
+    return this.permissionGranted.includes(test)
+  }
+  /**
+   * Returns if it has many permissions
+   */
+  @exposeMethod
+  async hasManyPermissions(test: PermissionItem[]): Promise<boolean> {
+    for (const item of test) {
+      if (!(await this.hasPermission(item))) {
+        return false
+      }
+    }
+    return true
+  }
+
+  /**
+   * Reset all permissions granted
+   */
+  resetPermissions() {
+    this.permissionGranted = []
+  }
+}
