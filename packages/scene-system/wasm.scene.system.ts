52573e698a499fa751c2a9403d85b48b35c7c5a4
Leandro Mendoza <leandro@decentraland.org>
2021-12-30 11:13:03 -0300

    back to branch wasm url

--- packages/scene-system/wasm.scene.system.ts
+++ packages/scene-system/wasm.scene.system.ts
@@ -39,7 +39,8 @@ class WebWorkerScene extends SceneRuntime {
     if (isWasmScene) {
       wasmBytes = new Uint8Array(await sourceResponse.arrayBuffer())
     } else {
-      const quickJsWasmURL = 'http://127.0.0.1:7666/loader.wasm'
+      const quickJsWasmURL =
+        'https://sdk-team-cdn.decentraland.org/@dcl/wasm-quickjs-loader/branch/feat/organize-project/loader.wasm'
       const quicksJSLoaderWasm = await (await fetch(quickJsWasmURL)).arrayBuffer()
       wasmBytes = new Uint8Array(quicksJSLoaderWasm)
     }
