7db9a9aa0a555cde09ca51aa0493eec3249f6c08
gonpombo8 <gonzalo@decentraland.org>
2022-02-16 11:26:53 -0300

    add analytics types

--- packages/shared/analytics/index.ts
+++ packages/shared/analytics/index.ts
@@ -1,13 +1,14 @@
 import { DEBUG_ANALYTICS } from 'config'
 import { defaultLogger } from 'shared/logger'
 import { globalObservable } from '../observables'
+import { TrackEvents } from './types'
 
 export type SegmentEvent = {
   name: string
   data: string
 }
 
-export function trackEvent(eventName: string, eventData: Record<string, any>) {
+export function trackEvent<K extends keyof TrackEvents>(eventName: K, eventData: TrackEvents[K]) {
   if (DEBUG_ANALYTICS) {
     defaultLogger.info(`Tracking event "${eventName}": `, eventData)
   }
