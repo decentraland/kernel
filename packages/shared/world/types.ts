b5c97abff79b8f5cbf20ac9dbd41b2900790b262
Adrian <54064092+Suduck@users.noreply.github.com>
2022-04-14 16:59:00 +0200

    feat: biw kill portable experiences (#266)
    * feat: invalidate scenes when publishing
    
    * style: es lint
    
    * fix: build problems
    
    * style: add void
    
    Co-authored-by: Gon Pombo <gonzalo@decentraland.org>
    
    * style: better typescript
    
    Co-authored-by: Gon Pombo <gonzalo@decentraland.org>
    
    * feat: improve publish flow
    
    * fix: correct way to do it for multiple times
    
    * style: linter
    
    * fix: problem whith reloading the scenes
    
    * fix: scene loading when you invalidate all scenes
    
    * style: linter fix
    
    * style: better error handle
    
    * fix:linter
    
    * fix: support multi coord
    
    * style: linter
    
    * fix: fixed publish from panel
    
    * fix: publish from editor
    
    * style: better code style
    
    * fix: correct reload
    
    * style: linter
    
    * fix: array problem
    
    * style: better code
    
    * style: linter
    
    * feat: kill all portable experiences
    
    * style: better code style
    
    * refactor: change approach
    
    * fix: unit test
    
    * fix: unit test
    
    * fix: log
    
    Co-authored-by: Gon Pombo <gonzalo@decentraland.org>

--- packages/shared/world/types.ts
+++ packages/shared/world/types.ts
@@ -27,6 +27,7 @@ export type BuilderIsolatedPayload = {
    * Whether or not we want a single scene to enter isolated mode.
    */
   recreateScene?: boolean
+  killPortableExperiences?: boolean
 }
 
 export type StatefulWorkerOptions = {
