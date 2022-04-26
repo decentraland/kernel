2761dd62685c5fb7520125bc8046a4ff48ac2a20
Hugo Arregui <hugo@decentraland.org>
2022-04-26 11:45:06 -0300

    update resolver for testing

--- packages/shared/comms/v3/resolver.ts
+++ packages/shared/comms/v3/resolver.ts
@@ -24,13 +24,14 @@ export function resolveCommsV3Urls(realm: Realm): { pingUrl: string; wsUrl: stri
   if (realm.hostname === 'local') {
     server = 'http://127.0.0.1:5000'
   } else if (realm.hostname === 'remote') {
-    server = 'https://explorer-bff.decentraland.io'
+    // server = 'https://explorer-bff.decentraland.io'
+    server = 'https://peer-testing-2.decentraland.org/bff'
   } else {
     server = realm.hostname.match(/:\/\//) ? realm.hostname : 'https://' + realm.hostname
   }
 
   const pingUrl = new URL('./status', server).toString()
-  const wsUrl = httpToWs(new URL('./ws-bff', server).toString())
+  const wsUrl = httpToWs(new URL('./bff/ws-bff', server).toString())
 
   return { pingUrl, wsUrl }
 }
