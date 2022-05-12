21d029f04461e6c0924f5cc3511010f08fc9aca2
Santi Andrade <sandrade@decentraland.org>
2022-05-12 22:35:37 +0200

    feat: retry the friends initialization (#282)
    * Create NotifyAboutFriendsNotInitialized function
    
    * Create RETRY_FRIENDS_INITIALIZATION action
    
    * Update sagas.ts
    
    * Revert "Update sagas.ts"
    
    This reverts commit 146fb55dad29640d0d23ffaddc8472d49a8c9f94.
    
    * Update sagas.ts
    
    * Remove TODO comment
    
    * Remove RETRY flow
    
    * Add a retry system for the friends initialization
    
    * Fix lint
    
    * Fix lint
    
    * Improve the retry flow
    
    * Fix lint
    
    * Fix 'apply' call
    
    * Solve conflicts from last merge
    
    * Fix lint
    
    * Solve conflicts from last merge
    
    * enable reconnection and prevent multiplication of callbacks
    
    * Remove uneeded imports
    
    * fix friends logic
    
    * fix friends logic
    
    Co-authored-by: menduz <github@menduz.com>

--- packages/shared/friends/actions.ts
+++ packages/shared/friends/actions.ts
@@ -3,11 +3,16 @@ import { action } from 'typesafe-actions'
 import { FriendshipAction } from 'shared/types'
 
 import { FriendsState } from './types'
+import { SocialAPI } from 'dcl-social-client'
 
 export const UPDATE_FRIENDSHIP = 'Update friendship'
 export const updateFriendship = (_action: FriendshipAction, userId: string, incoming: boolean) =>
   action(UPDATE_FRIENDSHIP, { action: _action, userId }, { incoming })
 export type UpdateFriendship = ReturnType<typeof updateFriendship>
+
+export const SET_MATRIX_CLIENT = '[CHAT] Set Matrix client'
+export const setMatrixClient = (socialApi: SocialAPI) => action(SET_MATRIX_CLIENT, { socialApi })
+export type SetMatrixClient = ReturnType<typeof setMatrixClient>
 
 export const UPDATE_PRIVATE_MESSAGING = 'Update private messaging state'
 export const updatePrivateMessagingState = (state: FriendsState) => action(UPDATE_PRIVATE_MESSAGING, state)
