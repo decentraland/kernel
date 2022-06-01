0110514a49305d2a70975f6adc3ee9a77e8daba6
Leandro Mendoza <leandro@decentraland.org>
2022-06-01 16:21:03 -0300

    log

--- packages/shared/apis/host/SocialController.ts
+++ packages/shared/apis/host/SocialController.ts
@@ -4,11 +4,13 @@ import * as codegen from '@dcl/rpc/dist/codegen'
 
 import { SocialControllerServiceDefinition } from './../gen/SocialController'
 import { avatarMessageObservable } from 'shared/comms/peers'
+import defaultLogger from 'shared/logger'
 
 export function registerSocialControllerServiceServerImplementation(port: RpcServerPort<PortContext>) {
   codegen.registerService(port, SocialControllerServiceDefinition, async () => ({
     async init(_req, ctx) {
       avatarMessageObservable.add((event: any) => {
+        defaultLogger.log('avatarMessageObseravable', { id: 'AVATAR_OBSERVABLE', data: event })
         ctx.eventChannel.push({ id: 'AVATAR_OBSERVABLE', data: event }).catch((err) => ctx.DevTools.logger.error(err))
       })
       return {}
