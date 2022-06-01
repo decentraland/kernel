421df1e02f5d20a67e2a1137328de4e7af66568b
Leandro Mendoza <leandro@decentraland.org>
2022-06-01 15:08:12 -0300

    fix comms

--- packages/shared/apis/host/CommunicationsController.ts
+++ packages/shared/apis/host/CommunicationsController.ts
@@ -2,44 +2,11 @@ import {
   ICommunicationsController,
   subscribeParcelSceneToCommsMessages,
   unsubscribeParcelSceneToCommsMessages
-} from 'shared/comms/sceneSubscriptions'
-import { ExposableAPI } from 'shared/apis/ExposableAPI'
-import { EngineAPI } from 'shared/apis/EngineAPI'
-import { ParcelIdentity } from './ParcelIdentity'
-import { PeerInformation } from 'shared/comms/interface/types'
+} from './../../comms/sceneSubscriptions'
+
+import { PeerInformation } from './../../comms/interface/types'
+
 import { sendParcelSceneCommsMessage } from './../../comms'
-
-// @registerAPI('CommunicationsController')
-// export class CommunicationsController extends ExposableAPI implements ICommunicationsController {
-//   parcelIdentity = this.options.getAPIInstance(ParcelIdentity)
-//   engineAPI = this.options.getAPIInstance(EngineAPI)
-
-//   get cid() {
-//     return this.parcelIdentity.cid
-//   }
-
-//   constructor(options: APIOptions) {
-//     super(options)
-//     subscribeParcelSceneToCommsMessages(this)
-//   }
-
-//   apiWillUnmount() {
-//     // Unsubscribe this parcel from events
-//     unsubscribeParcelSceneToCommsMessages(this)
-//   }
-
-//   receiveCommsMessage(message: string, sender: PeerInformation) {
-//     this.engineAPI.sendSubscriptionEvent('comms', {
-//       message,
-//       sender: sender.uuid
-//     })
-//   }
-
-//   @exposeMethod
-//   async send(message: string): Promise<void> {
-//     sendParcelSceneCommsMessage(this.cid, message)
-//   }
-// }
 import { RpcServerPort } from '@dcl/rpc'
 import { PortContext } from './context'
 import * as codegen from '@dcl/rpc/dist/codegen'
@@ -47,21 +14,31 @@ import * as codegen from '@dcl/rpc/dist/codegen'
 import { CommunicationsControllerServiceDefinition } from './../gen/CommunicationsController'
 
 export function CommunicationsControllerServiceServerImplementation(port: RpcServerPort<PortContext>) {
+  let commsController: ICommunicationsController | null = null
+
   codegen.registerService(port, CommunicationsControllerServiceDefinition, async () => ({
     async init(_req, ctx) {
-      // todo: this is the subscribe of EngineAPI
-      if (!ctx.EngineAPI.subscribedEvents['comms']) {
-        ctx.EngineAPI.parcelSceneAPI.on('comms', (data: any) => {
-          if (ctx.EngineAPI.subscribedEvents['comms']) {
-            ctx.eventChannel.push({ id: 'comms', data }).catch((error) => ctx.DevTools.logger.error(error))
+      commsController = {
+        cid: ctx.ParcelIdentity.cid,
+        receiveCommsMessage(message: string, sender: PeerInformation) {
+          const data = {
+            message,
+            sender: sender.uuid
           }
-        })
-        ctx.EngineAPI.subscribedEvents['comms'] = true
+          ctx.eventChannel.push({ id: 'comms', data }).catch((error) => ctx.DevTools.logger.error(error))
+        }
       }
+
+      subscribeParcelSceneToCommsMessages(commsController)
       return {}
     },
-    async realSend(req) {
-      sendParcelSceneCommsMessage(this.cid, req.message)
+    async dispose() {
+      if (commsController !== null) {
+        unsubscribeParcelSceneToCommsMessages(commsController)
+      }
+    },
+    async realSend(req, ctx) {
+      sendParcelSceneCommsMessage(ctx.ParcelIdentity.cid, req.message)
       return {}
     }
   }))
