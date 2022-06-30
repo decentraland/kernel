41880b535956171890a75133eb6d8e756dcd89ae
Pato <pato@decentraland.org>
2022-06-30 15:59:02 -0300

    clean code

--- packages/renderer-protocol/services/crdtService.ts
+++ packages/renderer-protocol/services/crdtService.ts
@@ -7,20 +7,3 @@ export function registerCRDTService<Context>(
 ): codegen.RpcClientModule<CRDTServiceDefinition, Context> {
   return codegen.loadService<Context, CRDTServiceDefinition>(clientPort, CRDTServiceDefinition)
 }
-
-// export function registerCRDTService2<Context>(clientPort: RpcClientPort) {
-//   const crdtService = codegen.loadService<Context, CRDTServiceDefinition>(clientPort, CRDTServiceDefinition)
-
-//   async function send(request: CRDTManyMessages) {
-//     return crdtService.sendCRDT(request)
-//   }
-
-//   async function notificationStream(request: CRDTStreamRequest) {
-//     return crdtService.cRDTNotificationStream(request)
-//   }
-
-//   return {
-//     send,
-//     notificationStream
-//   }
-// }
