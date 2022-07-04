4a836139fa39c63bdd0236d3076180876b4b6223
jmoguilevsky <jmoguilevsky@gmail.com>
2022-07-04 19:26:50 -0300

    return original profile as promise function

--- packages/shared/profiles/ProfileAsPromise.ts
+++ packages/shared/profiles/ProfileAsPromise.ts
@@ -1,5 +1,5 @@
 import { getProfile, getProfileStatusAndData } from './selectors'
-import { profilesRequest } from './actions'
+import { profileRequest, profilesRequest } from './actions'
 import { ProfileType } from './types'
 import { COMMS_PROFILE_TIMEOUT } from 'config'
 import { store } from 'shared/store/isolatedStore'
@@ -10,6 +10,58 @@ const PROFILE_SOFT_TIMEOUT_MS = 5000
 
 // We reject the profile promise if more time than this has passed
 const PROFILE_HARD_TIMEOUT_MS = COMMS_PROFILE_TIMEOUT + 20000
+
+export function ProfileAsPromise(userId: string, version?: number, profileType?: ProfileType): Promise<Avatar> {
+  function isExpectedVersion(aProfile: Avatar) {
+    return !version || aProfile.version >= version
+  }
+
+  const [, existingProfile] = getProfileStatusAndData(store.getState(), userId)
+  const existingProfileWithCorrectVersion = existingProfile && isExpectedVersion(existingProfile)
+  if (existingProfile && existingProfileWithCorrectVersion) {
+    return Promise.resolve(existingProfile)
+  }
+  return new Promise<Avatar>((resolve, reject) => {
+    let pending = true
+    const unsubscribe = store.subscribe(() => {
+      const [status, data] = getProfileStatusAndData(store.getState(), userId)
+
+      if (status === 'error') {
+        unsubscribe()
+        pending = false
+        return reject(data)
+      }
+
+      const profile = getProfile(store.getState(), userId)
+      if (profile && isExpectedVersion(profile) && status === 'ok') {
+        unsubscribe()
+        pending = false
+        return resolve(profile)
+      }
+    })
+    store.dispatch(profileRequest(userId, profileType, version))
+
+    setTimeout(() => {
+      if (pending) {
+        const profile = getProfile(store.getState(), userId)
+
+        if (profile) {
+          unsubscribe()
+          pending = false
+          resolve(profile)
+        } else {
+          setTimeout(() => {
+            if (pending) {
+              unsubscribe()
+              pending = false
+              reject(new Error(`Timed out trying to resolve profile ${userId} (version: ${version})`))
+            }
+          }, PROFILE_HARD_TIMEOUT_MS - PROFILE_SOFT_TIMEOUT_MS)
+        }
+      }
+    }, PROFILE_SOFT_TIMEOUT_MS)
+  })
+}
 
 export function ProfilesAsPromise(userIds: string[], version?: number, profileType?: ProfileType): Promise<Avatar[]> {
   function isExpectedVersion(aProfile: Avatar) {
