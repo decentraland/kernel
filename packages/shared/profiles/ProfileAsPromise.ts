54eaebf865fb7e20c1697a5e194bc1dd3077b25d
moliva <oliva.miguelh@gmail.com>
2022-09-05 17:24:44 -0300

    feat: extracting profiles as promise feat to a separate branch

--- packages/shared/profiles/ProfileAsPromise.ts
+++ packages/shared/profiles/ProfileAsPromise.ts
@@ -1,9 +1,13 @@
-import { getProfile, getProfileStatusAndData } from './selectors'
-import { profileRequest } from './actions'
-import { ProfileType } from './types'
+import { getProfile, getProfileStatusAndData, isAddedToCatalog } from './selectors'
+import { addedProfileToCatalog, profileRequest, profilesRequest } from './actions'
+import { ProfileType, REMOTE_AVATAR_IS_INVALID } from './types'
 import { COMMS_PROFILE_TIMEOUT } from 'config'
 import { store } from 'shared/store/isolatedStore'
 import { Avatar } from '@dcl/schemas'
+import { getUnityInstance } from 'unity-interface/IUnityInterface'
+import { profileToRendererFormat } from './transformations/profileToRendererFormat'
+import { ensureUnityInterface } from 'shared/renderer'
+import { isCurrentUserId } from 'shared/session/selectors'
 
 // We resolve a profile with an older version after this time, if there is that info
 const PROFILE_SOFT_TIMEOUT_MS = 5000
@@ -11,15 +15,22 @@ const PROFILE_SOFT_TIMEOUT_MS = 5000
 // We reject the profile promise if more time than this has passed
 const PROFILE_HARD_TIMEOUT_MS = COMMS_PROFILE_TIMEOUT + 20000
 
-export function ProfileAsPromise(userId: string, version?: number, profileType?: ProfileType): Promise<Avatar> {
+// This method creates a promise that makes sure that a profile was downloaded AND added to renderer's catalog
+export async function ProfileAsPromise(userId: string, version?: number, profileType?: ProfileType): Promise<Avatar> {
   function isExpectedVersion(aProfile: Avatar) {
     return !version || aProfile.version >= version
   }
 
   const [, existingProfile] = getProfileStatusAndData(store.getState(), userId)
   const existingProfileWithCorrectVersion = existingProfile && isExpectedVersion(existingProfile)
-  if (existingProfile && existingProfileWithCorrectVersion) {
-    return Promise.resolve(existingProfile)
+  const isInCatalog = isAddedToCatalog(store.getState(), userId)
+  if (existingProfile && existingProfileWithCorrectVersion && !isCurrentUserId(store.getState(), userId)) {
+    if (!isInCatalog) {
+      await ensureUnityInterface()
+      getUnityInstance().AddUserProfileToCatalog(profileToRendererFormat(existingProfile, {}))
+      store.dispatch(addedProfileToCatalog(userId, existingProfile))
+    }
+    return existingProfile
   }
   return new Promise<Avatar>((resolve, reject) => {
     let pending = true
@@ -33,7 +44,7 @@ export function ProfileAsPromise(userId: string, version?: number, profileType?:
       }
 
       const profile = getProfile(store.getState(), userId)
-      if (profile && isExpectedVersion(profile) && status === 'ok') {
+      if (profile && isExpectedVersion(profile) && status === 'ok' && isAddedToCatalog(store.getState(), userId)) {
         unsubscribe()
         pending = false
         return resolve(profile)
@@ -58,6 +69,86 @@ export function ProfileAsPromise(userId: string, version?: number, profileType?:
             }
           }, PROFILE_HARD_TIMEOUT_MS - PROFILE_SOFT_TIMEOUT_MS)
         }
+      }
+    }, PROFILE_SOFT_TIMEOUT_MS)
+  })
+}
+
+// This method creates a promise that makes sure that an array of profiles were downloaded
+// but they will not be added to renderer's catalog
+export function ProfilesAsPromise(userIds: string[], profileType?: ProfileType): Promise<Avatar[]> {
+  const usersToFech = userIds.filter((userId) => {
+    const [, existingProfile] = getProfileStatusAndData(store.getState(), userId)
+    const existingProfileWithCorrectVersion = existingProfile
+
+    // if it already exists we don't want to fetch it
+    return !(existingProfile && existingProfileWithCorrectVersion)
+  })
+
+  let pending = true
+
+  return new Promise<Avatar[]>((resolve, reject) => {
+    let failedAvatars = 0
+
+    const unsubscribe = store.subscribe(() => {
+      const avatars: Avatar[] = []
+      failedAvatars = 0
+
+      for (const userId of userIds) {
+        const [status, data] = getProfileStatusAndData(store.getState(), userId)
+
+        if (status === 'error') {
+          // If it's an error the data can be a string
+          if ((data as any) === REMOTE_AVATAR_IS_INVALID) {
+            failedAvatars += 1
+          } else {
+            unsubscribe()
+            pending = false
+            return reject(data)
+          }
+        }
+
+        const profile = getProfile(store.getState(), userId)
+        if (profile && status === 'ok') {
+          avatars.push(profile)
+        }
+      }
+
+      if (avatars.length + failedAvatars === userIds.length) {
+        unsubscribe()
+        pending = false
+        resolve(avatars)
+      }
+    })
+
+    if (usersToFech.length > 0) {
+      store.dispatch(profilesRequest(usersToFech, profileType))
+    }
+
+    setTimeout(() => {
+      // if it's pending it means that the promise has already resolved since none's clearing the timeout
+      if (!pending) {
+        return
+      }
+      const profiles = userIds
+        .map((userId) => getProfile(store.getState(), userId))
+        .filter((profile) => !!profile) as Avatar[]
+
+      if (profiles.length + failedAvatars === userIds.length) {
+        unsubscribe()
+        pending = false
+        resolve(profiles)
+      } else {
+        setTimeout(() => {
+          // if it's pending it means that the promise has already resolved since none's clearing the timeout
+          if (!pending) {
+            return
+          }
+
+          unsubscribe()
+          pending = false
+          reject(new Error(`Timed out trying to resolve profiles ${userIds}`))
+        }, PROFILE_HARD_TIMEOUT_MS - PROFILE_SOFT_TIMEOUT_MS)
       }
     }, PROFILE_SOFT_TIMEOUT_MS)
   })
--- packages/shared/profiles/actions.ts
+++ packages/shared/profiles/actions.ts
@@ -8,6 +8,10 @@ export const PROFILE_REQUEST = '[PROFILE] Fetch request'
 export const PROFILE_SUCCESS = '[PROFILE] Fetch succeeded'
 export const PROFILE_FAILURE = '[PROFILE] Fetch failed'
 
+export const PROFILES_REQUEST = '[PROFILES] Fetch request'
+export const PROFILES_SUCCESS = '[PROFILES] Fetch succeeded'
+export const PROFILES_FAILURE = '[PROFILES] Fetch failed'
+
 export const SAVE_PROFILE = 'SAVE_PROFILE'
 export const SAVE_PROFILE_FAILURE = 'SAVE_PROFILE_FAILURE'
 
@@ -20,6 +24,9 @@ export const SEND_PROFILE_TO_RENDERER = 'SEND_PROFILE_TO_RENDERER'
 export const profileRequest = (userId: string, profileType?: ProfileType, version?: number) =>
   action(PROFILE_REQUEST, { userId, profileType, version })
 
+export const profilesRequest = (userIds: string[], profileType?: ProfileType) =>
+  action(PROFILES_REQUEST, { userIds, profileType })
+
 /**
  * profileSuccess stores locally a profile and sends it to the renderer.
  * It can be the result of either a profileRequest or local profile loading/editing
@@ -30,6 +37,13 @@ export const profileFailure = (userId: string, error: any) => action(PROFILE_FAI
 export type ProfileRequestAction = ReturnType<typeof profileRequest>
 export type ProfileSuccessAction = ReturnType<typeof profileSuccess>
 export type ProfileFailureAction = ReturnType<typeof profileFailure>
+
+export const profilesSuccess = (profiles: Avatar[]) => action(PROFILES_SUCCESS, { profiles })
+export const profilesFailure = (userIds: string[], error: any) => action(PROFILES_FAILURE, { userIds, error })
+
+export type ProfilesRequestAction = ReturnType<typeof profilesRequest>
+export type ProfilesSuccessAction = ReturnType<typeof profilesSuccess>
+export type ProfilesFailureAction = ReturnType<typeof profilesFailure>
 
 // Profile update
 
@@ -54,6 +68,10 @@ export const ADDED_PROFILE_TO_CATALOG = '[Success] Added profile to catalog'
 export const addedProfileToCatalog = (userId: string, profile: Avatar) =>
   action(ADDED_PROFILE_TO_CATALOG, { userId, profile })
 export type AddedProfileToCatalog = ReturnType<typeof addedProfileToCatalog>
+
+export const ADDED_PROFILES_TO_CATALOG = '[Success] Added profiles to catalog'
+export const addedProfilesToCatalog = (profiles: Avatar[]) => action(ADDED_PROFILES_TO_CATALOG, { profiles })
+export type AddedProfilesToCatalog = ReturnType<typeof addedProfilesToCatalog>
 
 // Profiles over comms
 export const PROFILE_RECEIVED_OVER_COMMS = 'PROFILE_RECEIVED_OVER_COMMS'
