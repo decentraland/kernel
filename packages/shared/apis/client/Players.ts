5055e6f5de5e71765334d87b35ea51ae3983c656
Lean Mendoza <leandro@decentraland.org>
2022-09-06 09:25:10 -0300

    feat: divide to real and legacy api service client (#464)
    * discriminate real vs legacy api service client
    
    * fix regex
    
    * add autogenerated apis
    
    * move bootstrap (SceneRuntime) to real service
    
    * ~rpc to ~system
    
    * add comment and type

--- packages/shared/apis/client/Players.ts
+++ packages/shared/apis/client/Players.ts
@@ -3,41 +3,47 @@ import { RpcClientPort } from '@dcl/rpc/dist/types'
 import { UserData } from '../../types'
 import { PlayersServiceDefinition } from '../proto/Players.gen'
 
-export function createPlayersServiceClient<Context>(clientPort: RpcClientPort) {
-  const originalService = codegen.loadService<Context, PlayersServiceDefinition>(clientPort, PlayersServiceDefinition)
+export namespace PlayersServiceClient {
+  export function create<Context>(clientPort: RpcClientPort) {
+    return codegen.loadService<Context, PlayersServiceDefinition>(clientPort, PlayersServiceDefinition)
+  }
 
-  return {
-    ...originalService,
-    /**
-     * Return the players's data
-     */
-    async getPlayerData(opt: { userId: string }): Promise<UserData | null> {
-      const originalResponse = await originalService.getPlayerData({ userId: opt.userId })
-      if (!originalResponse.data) {
-        return null
+  export function createLegacy<Context>(clientPort: RpcClientPort) {
+    const originalService = codegen.loadService<Context, PlayersServiceDefinition>(clientPort, PlayersServiceDefinition)
+
+    return {
+      ...originalService,
+      /**
+       * Return the players's data
+       */
+      async getPlayerData(opt: { userId: string }): Promise<UserData | null> {
+        const originalResponse = await originalService.getPlayerData({ userId: opt.userId })
+        if (!originalResponse.data) {
+          return null
+        }
+        return {
+          ...originalResponse.data,
+          avatar: {
+            ...originalResponse.data.avatar!,
+            snapshots: originalResponse.data.avatar!.snapshots!
+          },
+          publicKey: originalResponse.data.publicKey || null
+        }
+      },
+
+      /**
+       * Return array of connected players
+       */
+      async getConnectedPlayers(): Promise<{ userId: string }[]> {
+        return (await originalService.getConnectedPlayers({})).players
+      },
+
+      /**
+       * Return array of players inside the scene
+       */
+      async getPlayersInScene(): Promise<{ userId: string }[]> {
+        return (await originalService.getPlayersInScene({})).players
       }
-      return {
-        ...originalResponse.data,
-        avatar: {
-          ...originalResponse.data.avatar!,
-          snapshots: originalResponse.data.avatar!.snapshots!
-        },
-        publicKey: originalResponse.data.publicKey || null
-      }
-    },
-
-    /**
-     * Return array of connected players
-     */
-    async getConnectedPlayers(): Promise<{ userId: string }[]> {
-      return (await originalService.getConnectedPlayers({})).players
-    },
-
-    /**
-     * Return array of players inside the scene
-     */
-    async getPlayersInScene(): Promise<{ userId: string }[]> {
-      return (await originalService.getPlayersInScene({})).players
     }
   }
 }
