c7971fb4e0f459e743a82b0168672c5538ff5d38
moliva <oliva.miguelh@gmail.com>
2022-09-07 14:17:18 -0300

    refactor: roll back change

--- packages/shared/friends/ensureFriendProfile.ts
+++ packages/shared/friends/ensureFriendProfile.ts
@@ -2,6 +2,6 @@ import { Avatar } from '@dcl/schemas'
 import { ProfileAsPromise } from 'shared/profiles/ProfileAsPromise'
 import { ProfileType } from 'shared/profiles/types'
 
-export async function ensureFriendProfile(userId: string): Promise<Avatar> {
+export function ensureFriendProfile(userId: string): Promise<Avatar> {
   return ProfileAsPromise(userId, undefined, ProfileType.DEPLOYED) // Friends are always deployed ATM
 }
