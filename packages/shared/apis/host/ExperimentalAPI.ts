d18ee75d7a25e7c2d3023bef1b90c9b2286cc424
menduz <github@menduz.com>
2022-09-10 17:23:04 -0300

    update @dcl/protocol

--- packages/shared/apis/host/ExperimentalAPI.ts
+++ packages/shared/apis/host/ExperimentalAPI.ts
@@ -10,12 +10,12 @@ export function registerExperimentalAPIServiceServerImplementation(port: RpcServ
   codegen.registerService(port, ExperimentalAPIServiceDefinition, async () => ({
     async sendToRenderer(req, ctx) {
       const protocol = await rendererProtocol
-      return protocol.crdtService.sendCrdt({ sceneId: ctx.sceneData.id, payload: req.data })
+      return protocol.crdtService.sendCrdt({ sceneId: ctx.sceneData.id, payload: req.data, sceneNumber: 0xffffffff })
     },
 
     async messageFromRenderer(_, ctx) {
       const protocol = await rendererProtocol
-      const response = await protocol.crdtService.pullCrdt({ sceneId: ctx.sceneData.id })
+      const response = await protocol.crdtService.pullCrdt({ sceneId: ctx.sceneData.id, sceneNumber: 0xffffffff })
       return { data: [response.payload] }
     }
   }))
