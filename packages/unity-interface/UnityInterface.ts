52211e0e4882481b555cffb5b43a37ba565c98d0
Alejandro Jimenez <ajimenez@decentraland.org>
2022-09-26 15:42:04 +0200

    chore: remove naive chat sanitization

--- packages/unity-interface/UnityInterface.ts
+++ packages/unity-interface/UnityInterface.ts
@@ -328,11 +328,6 @@ export class UnityInterface implements IUnityInterface {
   }
 
   public AddMessageToChatWindow(message: ChatMessage) {
-    try {
-      message.body = message.body.replace(/</g, 'ᐸ').replace(/>/g, 'ᐳ')
-    } catch (err: any) {
-      unityLogger.error(err)
-    }
     if (message.body.length > 1000) {
       trackEvent('long_chat_message_ignored', { message: message.body, sender: message.sender })
       return
