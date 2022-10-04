f8996b4a995e118aebdf2f0e1f4afa45558b95e7
Lean Mendoza <leandro@decentraland.org>
2022-10-04 14:39:52 -0300

    feat: add content mapping to playground (#565)
    * add content mapping to playground
    
    * add baseurl config
    
    * fix typo

--- packages/unity-interface/dcl.ts
+++ packages/unity-interface/dcl.ts
@@ -31,7 +31,7 @@ import type { UnityGame } from '@dcl/unity-renderer/src'
 import { fetchScenesByLocation } from 'decentraland-loader/lifecycle/utils/fetchSceneIds'
 import { traceDecoratorUnityGame } from './trace'
 import defaultLogger from 'shared/logger'
-import { EntityType, Scene, sdk } from '@dcl/schemas'
+import { ContentMapping, EntityType, Scene, sdk } from '@dcl/schemas'
 import { ensureMetaConfigurationInitialized } from 'shared/meta'
 import { reloadScenePortableExperience } from 'shared/portableExperiences/actions'
 import { ParcelSceneLoadingParams } from 'decentraland-loader/lifecycle/manager'
@@ -91,7 +91,13 @@ export async function initializeEngine(_gameInstance: UnityGame): Promise<void> 
   }
 }
 
-async function startGlobalScene(cid: string, title: string, fileContentUrl: string) {
+async function startGlobalScene(
+  cid: string,
+  title: string,
+  fileContentUrl: string,
+  content: ContentMapping[] = [],
+  baseUrl: string = location.origin
+) {
   const metadata: Scene = {
     display: {
       title: title
@@ -105,9 +111,9 @@ async function startGlobalScene(cid: string, title: string, fileContentUrl: stri
 
   const scene = loadParcelSceneWorker({
     id: cid,
-    baseUrl: location.origin,
+    baseUrl,
     entity: {
-      content: [{ file: 'game.js', hash: fileContentUrl }],
+      content: [...content, { file: 'game.js', hash: fileContentUrl }],
       pointers: [cid],
       timestamp: 0,
       type: EntityType.SCENE,
@@ -210,6 +216,8 @@ export async function loadPreviewScene(message: sdk.Messages) {
 
 export async function reloadPlaygroundScene() {
   const playgroundCode: string = (globalThis as any).PlaygroundCode
+  const playgroundContentMapping: ContentMapping[] = (globalThis as any).PlaygroundContentMapping || []
+  const playgroundBaseUrl: string = (globalThis as any).PlaygroundBaseUrl || location.origin
 
   if (!playgroundCode) {
     console.log('There is no playground code')
@@ -223,7 +231,7 @@ export async function reloadPlaygroundScene() {
 
   const hudWorkerBLOB = new Blob([playgroundCode])
   const hudWorkerUrl = URL.createObjectURL(hudWorkerBLOB)
-  await startGlobalScene(sceneId, 'SDK Playground', hudWorkerUrl)
+  await startGlobalScene(sceneId, 'SDK Playground', hudWorkerUrl, playgroundContentMapping, playgroundBaseUrl)
 }
 
 teleportObservable.add((position: { x: number; y: number; text?: string }) => {
