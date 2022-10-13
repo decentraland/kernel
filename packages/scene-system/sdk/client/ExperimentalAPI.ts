f531f70a51eb4e98ba047d8c3e49a1817f173596
menduz <github@menduz.com>
2022-10-13 13:35:51 -0300

    remove scene system

--- packages/scene-system/sdk/client/ExperimentalAPI.ts
+++
@@ -1,7 +0,0 @@
-import * as codegen from '@dcl/rpc/dist/codegen'
-import { RpcClientPort } from '@dcl/rpc/dist/types'
-import { ExperimentalAPIServiceDefinition } from '../../../shared/protocol/kernel/apis/ExperimentalAPI.gen'
-
-export function createExperimentalAPIServiceClient<Context>(clientPort: RpcClientPort) {
-  return codegen.loadService<Context, ExperimentalAPIServiceDefinition>(clientPort, ExperimentalAPIServiceDefinition)
-}
