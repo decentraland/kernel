b04be234fc454e0374bf2bec0713f93d6067e4e2
Juli Milloch <millochjuli@gmail.com>
2022-10-21 15:20:11 -0300

    refactor: use existing `ChannelMember` type

--- packages/shared/types.ts
+++ packages/shared/types.ts
@@ -837,11 +837,6 @@ export type ChannelMember = {
   isOnline?: boolean
 }
 
-export type ChannelMemberProfile = {
-  userId: string
-  name: string
-}
-
 export type UpdateChannelMembersPayload = {
   channelId: string
   members: ChannelMember[]
