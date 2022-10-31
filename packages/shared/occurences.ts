c361490194683edfde50afb503d4dced85a883cd
menduz <github@menduz.com>
2022-10-31 20:20:36 -0300

    feat: add ping counter

--- packages/shared/occurences.ts
+++ packages/shared/occurences.ts
@@ -1,6 +1,23 @@
-const occurrencesCounter = new Map<string, number>()
-export function incrementCounter(counter: string) {
-  occurrencesCounter.set(counter, (occurrencesCounter.get(counter) || 0) + 1)
+const occurrencesCounter = new Map<Counters, number>()
+
+export type Counters =
+  | 'pong_duplicated_response_counter'
+  | 'pong_expected_counter'
+  | 'pong_given_counter'
+  | 'failed:sendPositionMessage'
+  | `commMessage:${string}`
+  | `setThrew:${string}`
+  | 'voiceChatHandlerError'
+  | 'voiceChatRequestMediaDeviceFail'
+  | 'ping_sent_counter'
+  | 'pong_sent_counter'
+  | 'pong_received_counter'
+  | 'ping_received_twice_counter'
+  | 'profile-over-comms-succesful'
+  | 'profile-over-comms-failed'
+
+export function incrementCounter(counter: Counters, by = 1) {
+  occurrencesCounter.set(counter, (occurrencesCounter.get(counter) || 0) + by)
 }
 
 export function getAndClearOccurenceCounters() {
