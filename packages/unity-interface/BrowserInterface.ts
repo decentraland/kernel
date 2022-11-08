a6a991f674ff0c574b8cf95c90521033eff5b311
Vitaly Popuzin <popuzin@gmail.com>
2022-11-08 18:48:19 +0100

    add comment for PR

--- packages/unity-interface/BrowserInterface.ts
+++ packages/unity-interface/BrowserInterface.ts
@@ -255,6 +255,7 @@ export class BrowserInterface {
    * and independant workflows for both teams.
    */
   public handleUnityMessage(type: string, message: any) {
+    // comment
     if (type in this) {
       ;(this as any)[type](message)
     } else {
