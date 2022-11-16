83faef01f75c73cfc4bc1c55a7ae1cbf91333f51
guidota <guido@tamborindeguy.com.ar>
2022-11-16 15:45:58 -0300

    fix: make queryGraph calls to be POST (#716)

--- packages/shared/web3.ts
+++ packages/shared/web3.ts
@@ -391,6 +391,7 @@ export async function fetchENSOwnersContains(url: string, name: string, maxResul
 
 async function queryGraph(url: string, query: string, variables: any, totalAttempts: number = 5) {
   const ret = await fetch(url, {
+    method: 'POST',
     body: JSON.stringify({ query, variables }),
     headers: { 'Content-Type': 'application/json' }
   })
