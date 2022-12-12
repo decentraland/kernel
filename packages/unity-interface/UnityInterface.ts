9282922cb1232b0c4f2356c6866c211171ac04d2
Juan Molteni <juanignaciomolteni@gmail.com>
2022-12-11 23:58:51 -0300

    Lint fix

--- packages/unity-interface/UnityInterface.ts
+++ packages/unity-interface/UnityInterface.ts
@@ -279,7 +279,7 @@ export class UnityInterface implements IUnityInterface {
   public AddWearablesToCatalog(wearables: WearableV2[], context?: string) {
     const jsonString = JSON.stringify({ wearables, context })
     const result = jsonString.match(/.{1,1306299}/g) || []
-    for(var i = 0;i<result.length;i++){
+    for (let i = 0; i < result.length; i++) {
       this.SendMessageToUnity('Main', 'AddWearablesToCatalog', result[i])
     }
   }
