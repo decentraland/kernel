7a39cb2fa079ef678b39a58b361c577cfd92f185
jmilloch <millochjuli@gmail.com>
2022-12-12 17:07:19 -0300

    feat: modify validation statement

--- packages/shared/friends/sagas.ts
+++ packages/shared/friends/sagas.ts
@@ -1325,19 +1325,14 @@ function* handleUpdateFriendship({ payload, meta }: UpdateFriendship) {
       } else {
         yield call(handleOutgoingUpdateFriendshipStatus, payload)
       }
+    }
 
-      // TODO!: remove FF validation once the new flow is the only one
-      // We only send the UpdateFriendshipStatus message when:
-      // + The new friend request flow is disabled
-      // + The new friend request flow is enabled and the action is an outgoing delete
-      // + It's an incoming update (except when the new friend request flow is enabled and the action is REQUESTED_FROM)
-      if (
-        !newFriendRequestFlow ||
-        (newFriendRequestFlow && action === FriendshipAction.DELETED) ||
-        (newFriendRequestFlow && incoming && action !== FriendshipAction.REQUESTED_FROM)
-      ) {
-        getUnityInstance().UpdateFriendshipStatus(payload)
-      }
+    // TODO!: remove FF validation once the new flow is the only one
+    // We only send the UpdateFriendshipStatus message when:
+    // + The new friend request flow is disabled
+    // + The new friend request flow is enabled and the action is an outgoing delete
+    if (!newFriendRequestFlow || (newFriendRequestFlow && action === FriendshipAction.DELETED)) {
+      getUnityInstance().UpdateFriendshipStatus(payload)
     }
 
     if (!incoming) {
