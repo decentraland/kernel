6f32393f0c846aad6e82faf551e23dbe524544a2
Miguel Oliva <moliva@users.noreply.github.com>
2022-12-20 11:55:58 -0300

    refactor: generalize request handling flow (#788)

--- packages/renderer-protocol/inverseRpc/services/friendRequestService.ts
+++ packages/renderer-protocol/inverseRpc/services/friendRequestService.ts
@@ -10,68 +10,20 @@ import {
 } from '../../../shared/friends/sagas'
 import defaultLogger from '../../../shared/logger'
 import { FriendshipErrorCode } from '@dcl/protocol/out-ts/decentraland/renderer/common/friend_request_common.gen'
-import {
-  CancelFriendRequestReply,
-  FriendRequestKernelServiceDefinition,
-  GetFriendRequestsReply,
-  RejectFriendRequestReply,
-  SendFriendRequestReply
-} from '@dcl/protocol/out-ts/decentraland/renderer/kernel_services/friend_request_kernel.gen'
+import { FriendRequestKernelServiceDefinition } from '@dcl/protocol/out-ts/decentraland/renderer/kernel_services/friend_request_kernel.gen'
 
 export function registerFriendRequestKernelService(port: RpcServerPort<RendererProtocolContext>) {
   codegen.registerService(port, FriendRequestKernelServiceDefinition, async () => ({
     async getFriendRequests(req, _) {
-      try {
-        // Go get friend requests
-        const friendRequests = await getFriendRequestsProtocol(req)
-
-        // Build send friend request reply
-        const getFriendRequestsReply: GetFriendRequestsReply = buildResponse(friendRequests)
-
-        // Send response back to renderer
-        return getFriendRequestsReply
-      } catch (err) {
-        defaultLogger.error('Error while getting friend requests via rpc', err)
-
-        // Send response back to renderer
-        return buildErrorResponse()
-      }
+      return handleRequest(getFriendRequestsProtocol, req)
     },
 
     async sendFriendRequest(req, _) {
-      try {
-        // Handle send friend request
-        const sendFriendRequest = await requestFriendship(req)
-
-        // Build send friend request reply
-        const sendFriendRequestReply: SendFriendRequestReply = buildResponse(sendFriendRequest)
-
-        // Send response back to renderer
-        return sendFriendRequestReply
-      } catch (err) {
-        defaultLogger.error('Error while sending friend request via rpc', err)
-
-        // Send response back to renderer
-        return buildErrorResponse()
-      }
+      return handleRequest(requestFriendship, req)
     },
 
     async cancelFriendRequest(req, _) {
-      try {
-        // Handle cancel friend request
-        const cancelFriend = await cancelFriendRequest(req)
-
-        // Build cancel friend request reply
-        const cancelFriendRequestReply: CancelFriendRequestReply = buildResponse(cancelFriend)
-
-        // Send response back to renderer
-        return cancelFriendRequestReply
-      } catch (err) {
-        defaultLogger.error('Error while canceling friend request via rpc', err)
-
-        // Send response back to renderer
-        return buildErrorResponse()
-      }
+      return handleRequest(cancelFriendRequest, req)
     },
 
     async acceptFriendRequest(req, _) {
@@ -79,28 +31,14 @@ export function registerFriendRequestKernelService(port: RpcServerPort<RendererP
     },
 
     async rejectFriendRequest(req, _) {
-      try {
-        // Handle reject friend request
-        const rejectFriend = await rejectFriendRequest(req)
-
-        // Build reject friend request reply
-        const rejectFriendRequestReply: RejectFriendRequestReply = buildResponse(rejectFriend)
-
-        // Send response back to renderer
-        return rejectFriendRequestReply
-      } catch (err) {
-        defaultLogger.error('Error while rejecting friend request via rpc', err)
-
-        // Send response back to renderer
-        return buildErrorResponse()
-      }
+      return handleRequest(rejectFriendRequest, req)
     }
   }))
 }
 
 type FriendshipError = { message: { $case: 'error'; error: FriendshipErrorCode } }
 
-type ResponseType<T> = { reply?: T; error?: FriendshipErrorCode }
+type ResponseType<T> = { reply: NonNullable<T>; error: undefined } | { reply: undefined; error: FriendshipErrorCode }
 
 /**
  * Build friend requests error message to send to renderer.
@@ -119,7 +57,7 @@ function buildErrorResponse(error?: FriendshipErrorCode): FriendshipError {
  * Build friend requests success message to send to renderer.
  * @param reply - a FriendRequestReplyOk kind of type.
  */
-function wrapReply<T>(reply: T) {
+function wrapReply<T>(reply: NonNullable<T>) {
   return { message: { $case: 'reply' as const, reply } }
 }
 
@@ -135,5 +73,23 @@ function buildResponse<T>(friendRequest: ResponseType<T>) {
     return wrapReply(friendRequest.reply)
   } else {
     return buildErrorResponse(friendRequest.error)
+  }
+}
+
+async function handleRequest<T, U>(handler: (r: T) => Promise<ResponseType<U>>, req: T) {
+  try {
+    // Handle request
+    const internalResponse = await handler(req)
+
+    // Build friend request reply
+    const response = buildResponse(internalResponse)
+
+    // Send response back to renderer
+    return response
+  } catch (err) {
+    defaultLogger.error('Error while processing friend request via rpc', err)
+
+    // Send response back to renderer
+    return buildErrorResponse()
   }
 }
