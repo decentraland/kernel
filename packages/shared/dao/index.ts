ce1d75a5fb44b967f5fca2974eb60a6995fd23d5
Vitaly Popuzin <popuzin@gmail.com>
2022-12-26 20:45:57 +0100

    exrended logs

--- packages/shared/dao/index.ts
+++ packages/shared/dao/index.ts
@@ -209,7 +209,7 @@ export async function changeRealm(realmString: string, forceChange: boolean = fa
   }
 
   const catalystURL = new URL(realmConfig.baseUrl)
-  console.log("vv " + realmConfig.baseUrl);
+  console.log("vv " + realmString + " forceChange " + forceChange + " realmConfig " + realmConfig);
   if (!forceChange) {
     const denylistedCatalysts: string[] = getDisabledCatalystConfig(store.getState()) ?? []
     if (denylistedCatalysts.find((denied) => new URL(denied).host === catalystURL.host)) {
