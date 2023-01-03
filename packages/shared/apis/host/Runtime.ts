aefe8d6933524a4d76e901b4cbb779d291f48793
gonpombo8 <gonzalo@decentraland.org>
2023-01-03 19:59:22 -0300

    update partial @dcl/scene-runtime

--- packages/shared/apis/host/Runtime.ts
+++ packages/shared/apis/host/Runtime.ts
@@ -11,9 +11,7 @@ import { PortContextService } from './context'
 import { getDecentralandTime } from './EnvironmentAPI'
 import { urlWithProtocol } from '../../realm/resolver'
 
-export function registerEnvironmentApiServiceServerImplementation(
-  port: RpcServerPort<PortContextService<'sceneData'>>
-) {
+export function registerRuntimeServiceServerImplementation(port: RpcServerPort<PortContextService<'sceneData'>>) {
   codegen.registerService(port, RuntimeServiceDefinition, async () => ({
     async getWorldTime(): Promise<GetWorldTimeResponse> {
       const time = getDecentralandTime()
